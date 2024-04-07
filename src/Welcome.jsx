d00c7a6ee15db0af6ac71c581ee5a2ec61482def
Mohammad Amber Alam <amberalam.3101@gmail.com>
2024-04-07 19:04:51 +0530

    fixed welcome msg

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Welcome({ isLoggedIn }) {
-  return <p>Welcome, user! </p>;
+function Welcome() {
+  return <p>Welcome, user</p>;
 }
 
 export default Welcome;
